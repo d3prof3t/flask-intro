7472ea1d23572cb2307ee412839af94e679d2bc1
Saurabh Sharma <saurabhpysharma@gmail.com>
2016-03-31 00:29:38 +0530

    APIs POC for all get/post/put/delete methods using Flask-Restful

--- flasktaskr/tasks/views.py
+++ flasktaskr/tasks/views.py
@@ -17,7 +17,7 @@ from flask_restful import Resource, Api, reqparse, fields, marshal_with
 
 # App Specific Imports
 from flasktaskr import db
-from .forms import AddTaskForm
+# from .forms import AddTaskForm
 from flasktaskr.models import Task
 
 
@@ -43,6 +43,7 @@ tasks = Api(tasks_blueprint)
 
 # Fields to be serialized
 resource_fields = {
+    'task_id': fields.Integer,
     'name': fields.String,
     'due_date': fields.String,
     'priority': fields.Integer,
@@ -58,10 +59,10 @@ class Tasks(Resource):
     def get(self):
         # Handling Request Params
         parser = reqparse.RequestParser()
-        # adding get args
+
         parser.add_argument('status_id', required=True, action='append', \
                 type=int, location='args', help='Specify type of tasks')
-        args = parser.parse_args(strict=True)
+        args = parser.parse_args()
 
         if 1 in args['status_id']:
             # get open tasks
@@ -82,21 +83,31 @@ class Tasks(Resource):
             return open_tasks
         elif not open_tasks and closed_tasks:
             return closed_tasks
+        else:
+            all_tasks = []
+            all_tasks.append(open_tasks)
+            all_tasks.append(closed_tasks)
+            return all_tasks
+            
 
 
     @marshal_with(resource_fields)
     def post(self):
-        # Handling Request Params
+        
+        # instantiate reqparse object
         parser = reqparse.RequestParser()
-        # adding post payload
+        
+        # payload data sanity check
         parser.add_argument('name', required=True, location='form')
         parser.add_argument('due_date', required=True, location='form')
         parser.add_argument('priority', required=True, type=int, \
                 location='form')
         parser.add_argument('user_id', required=True, type=int, \
                 location='form')
-        # Creating args objecto
+
+        # creating args object
         args = parser.parse_args(strict=True)
+        
         # create new task object to be inserted
         task = Task(
             args['name'],
@@ -115,95 +126,64 @@ class Tasks(Resource):
         return task
 
 
+    @marshal_with(resource_fields)
+    def put(self):
+
+        # instantiate reqparse object
+        parser = reqparse.RequestParser()
+
+        # payload data sanity check
+        parser.add_argument('task_id', required=True, type=int, \
+                location='form', help="Task ID is required")
+
+        # create the args object
+        args = parser.parse_args(strict=True)
+
+        # get the appropriate task
+        task = db.session.query(Task).filter_by(task_id=args['task_id']). \
+                first()
+        
+        # set the task staus as closed
+        task.status = 0
+
+        # update date modified
+        task.date_modified = datetime.datetime.now()
+        
+        # add the changes to the session
+        db.session.add(task)
+
+        # commit the changes to the db
+        db.session.commit()
+
+        return task
+
+
+    @marshal_with(resource_fields)
+    def delete(self):
+
+        # instantiate the reqparse object
+        parser = reqparse.RequestParser()
+
+        # payload data sanity check
+        parser.add_argument('task_id', required=True, type=int, \
+                location='form', help="Task ID is required")
+
+        # create the args object
+        args = parser.parse_args(strict=True)
+
+        # get the appropriate task
+        task = db.session.query(Task).filter_by(task_id=args['task_id']). \
+                first()
+        
+        # delete the row from db
+        db.session.delete(task)
+
+        # commit the changes to the db
+        db.session.commit()
+
+        return task
+
+
+# Bind the resource to a route
 tasks.add_resource(Tasks, '/tasks', )
 
-# Routes
-
-
-# @tasks_blueprint.route('/tasks')
-# @login_required
-# def tasks():
-#     error = None
-#     form = AddTaskForm()
-#     # get all completed tasks
-#     closed_tasks = db.session.query(Task).filter_by(status=0).all()
-#     # get all open tasks
-#     open_tasks = db.session.query(Task).filter_by(status=1).all()
-
-#     # Data Integrity Check
-#     if not closed_tasks and not open_tasks:
-#         error = "No tasks found."
-#         return render_template('tasks.html', error=error, form=form)
-#     else:
-#         return render_template(
-#                     'tasks.html',
-#                     open_tasks=open_tasks,
-#                     closed_tasks = closed_tasks,
-#                     form=form
-#                 )
-
-
-# @tasks_blueprint.route('/add', methods=['POST'])
-# @login_required
-# def new_task():
-#     error = None
-
-#     # Get the basic fields from the form for creating a new task
-#     task_name = request.form['name']
-#     due_date = request.form['due_date']
-#     priority = request.form['priority']
-
-#     # Data Integrity Check
-#     if not task_name or not due_date or not priority:
-#         flash("All fields are required. Please fill in all the fields.")
-#         return redirect(url_for('tasks.tasks'))
-#     else:
-#         # Create the Python Object to be inserted
-#         task = Task (
-#             request.form['name'],
-#             request.form['due_date'],
-#             request.form['priority'],
-#             1,
-#             session['user_id'],
-#             datetime.datetime.now(),
-#             datetime.datetime.now()
-#         )
-#         # Add the changes to the Session
-#         db.session.add(task)
-#         # Commit the changes to the DB
-#         db.session.commit()
-#         # Redirect to /tasks with appropriate messages
-#         flash("New task was successfully created.")
-#         return redirect(url_for('tasks.tasks'))
-
-
-# @tasks_blueprint.route('/complete/<int:task_id>')
-# @login_required
-# def complete(task_id):
-#     # Get the appropriate task
-#     task = db.session.query(Task).filter_by(task_id=task_id).first()
-#     # Set the tasks status to closed
-#     task.status = 0
-#     # Update the date modified to current datetime
-#     task.date_modified = datetime.datetime.now()
-#     # Add the changes to the Session
-#     db.session.add(task)
-#     # Commit the changes to the DB
-#     db.session.commit()
-#     # Redirect to /tasks with appropriate message
-#     flash("The task was marked as complete.")
-#     return redirect(url_for('tasks.tasks'))
-
-
-# @tasks_blueprint.route('/delete/<int:task_id>')
-# @login_required
-# def delete(task_id):
-#     # Get the appropriate task
-#     task = db.session.query(Task).filter_by(task_id=task_id).first()
-#     # Delete the Row from DB
-#     db.session.delete(task)
-#     # Commit the changes to the DB
-#     db.session.commit()
-#     # Redirect to Tasks with appropriate messages
-#     flash("The task was successfully deleted")
-#     return redirect(url_for('tasks.tasks'))
