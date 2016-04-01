500ea0c682c553a51c3b46b2b5f6e64bf0c9243c
Saurabh Sharma <saurabhpysharma@gmail.com>
2016-04-02 01:37:09 +0530

    optimized api endpoints and SQLAlchemy db queries

--- flasktaskr/tasks/views.py
+++ flasktaskr/tasks/views.py
@@ -13,7 +13,7 @@ from flask import flash, redirect, url_for, session, request, \
         render_template, Blueprint
 
 # Flask Third party imports
-from flask_restful import Resource, Api, reqparse, fields, marshal_with
+from flask_restful import Resource, Api, reqparse, fields, marshal, marshal_with
 
 # App Specific Imports
 from flasktaskr import db
@@ -28,22 +28,9 @@ from flasktaskr.models import Task
 tasks_blueprint = Blueprint('tasks', __name__)
 tasks = Api(tasks_blueprint)
 
-# Helper Functions
-
-
-# def login_required(some_func):
-#     @wraps(some_func)
-#     def wrap(*args, **kwargs):
-#         if 'logged_in' in session:
-#             return some_func(*args, **kwargs)
-#         else:
-#             flash('You need to login first.')
-#             return redirect(url_for('users.login'))
-#     return wrap
 
 # Fields to be serialized
 resource_fields = {
-    'task_id': fields.Integer,
     'name': fields.String,
     'due_date': fields.String,
     'priority': fields.Integer,
@@ -53,46 +40,65 @@ resource_fields = {
     'date_modified': fields.DateTime
 }
 
-class Tasks(Resource):
 
-    @marshal_with(resource_fields)
-    def get(self):
-        # Handling Request Params
-        parser = reqparse.RequestParser()
+class AllTasks(Resource):
+    """
+    Returns all tasks
+    """
 
-        parser.add_argument('status_id', required=True, action='append', \
-                type=int, location='args', help='Specify type of tasks')
-        args = parser.parse_args()
-
-        if 1 in args['status_id']:
-            # get open tasks
-            open_tasks = db.session.query(Task).filter_by(status=1).all()
-        else:
-            open_tasks = []
-
-        if 0 in args['status_id']:
-            # get closed tasks
-            closed_tasks = db.session.query(Task).filter_by(status=0).all()
-        else:
-            closed_tasks = []
-
-        # prepare response
-        if not open_tasks and not closed_tasks:
-            return {'success': 'false', 'message': 'No tasks found'}
-        elif open_tasks and not closed_tasks:
-            return open_tasks
-        elif not open_tasks and closed_tasks:
-            return closed_tasks
-        else:
-            all_tasks = []
-            all_tasks.append(open_tasks)
-            all_tasks.append(closed_tasks)
-            return all_tasks
-
-
-    @marshal_with(resource_fields)
-    def post(self):
+    def get(self, status_id):
         
+        # get tasks based on status_id
+        tasks = Task.query.filter_by(status=status_id).all()
+
+        if not tasks:
+            return {
+                'success': 'false',
+                'message': 'No tasks found.'
+            }
+            
+        else:
+            return {
+                'success': 'true',
+                'data': marshal(tasks, resource_fields)
+            }
+
+
+
+class Tasks(Resource):
+    """
+    Single Task Resource
+    """
+
+    def get(self, task_id):
+
+        # check for task_id
+        if not task_id:
+            return {
+                'success': 'false',
+                'message': 'No task id specified.'
+            }
+        
+        else:
+            # get a single task
+            task = Task.query.get(task_id)
+            
+            if not task:
+                return {
+                    'success': 'false',
+                    'message': 'Task does not exists for task id => {}'. \
+                            format(task_id)
+                }
+
+            else:
+                return {
+                    'success': 'true',
+                    'data': marshal(task, resource_fields)
+                }
+
+
+    def post(self):
+
         # instantiate reqparse object
         parser = reqparse.RequestParser()
         
@@ -106,28 +112,39 @@ class Tasks(Resource):
 
         # creating args object
         args = parser.parse_args(strict=True)
+
+        try:
+            
+            # create new task object to be inserted
+            task = Task(
+                args['name'],
+                args['due_date'],
+                int(args['priority']),
+                1,
+                args['user_id'],
+                datetime.datetime.now(),
+                datetime.datetime.now()
+            )
+
+            # insert the new task
+            db.session.add(task)
+
+            # commit the changes
+            db.session.commit()
+
+            return {
+                'success': 'true',
+                'message': 'New task inserted.',
+                'data': marshal(task, resource_fields)
+            }, 201
         
-        # create new task object to be inserted
-        task = Task(
-            args['name'],
-            args['due_date'],
-            int(args['priority']),
-            1,
-            args['user_id'],
-            datetime.datetime.now(),
-            datetime.datetime.now()
-        )
+        except Exception as e:
+            return {
+                'success': 'false',
+                'message': '{}'.format(e)
+            }
+      
 
-        # insert the new task
-        db.session.add(task)
-
-        # commit the changes
-        db.session.commit()
-
-        return task
-
-
-    @marshal_with(resource_fields)
     def put(self):
 
         # instantiate reqparse object
@@ -140,26 +157,37 @@ class Tasks(Resource):
         # create the args object
         args = parser.parse_args(strict=True)
 
-        # get the appropriate task
-        task = db.session.query(Task).filter_by(task_id=args['task_id']). \
-                first()
+        try:
+
+            # get the appropriate task
+            task = db.session.query(Task).filter_by(task_id=args['task_id']). \
+                    first()
         
-        # set the task staus as closed
-        task.status = 0
+            # set the task staus as closed
+            task.status = 2
 
-        # update date modified
-        task.date_modified = datetime.datetime.now()
+            # update date modified
+            task.date_modified = datetime.datetime.now()
         
-        # add the changes to the session
-        db.session.add(task)
+            # add the changes to the session
+            db.session.add(task)
 
-        # commit the changes to the db
-        db.session.commit()
+            # commit the changes to the db
+            db.session.commit()
 
-        return task
+            return {
+                'success': 'true',
+                'message': 'Task updated successfully.',
+                'data': marshal(task, resource_fields)
+            }
+
+        except Exception as e:
+            return {
+                'success': 'false',
+                'message': '{}'.format(e)
+            }
 
 
-    @marshal_with(resource_fields)
     def delete(self):
 
         # instantiate the reqparse object
@@ -172,18 +200,30 @@ class Tasks(Resource):
         # create the args object
         args = parser.parse_args(strict=True)
 
-        # get the appropriate task
-        task = db.session.query(Task).filter_by(task_id=args['task_id']). \
-                first()
+        try:
+
+            # get the appropriate task
+            task = db.session.query(Task).filter_by(task_id=args['task_id']). \
+                    first()
         
-        # delete the row from db
-        db.session.delete(task)
+            # delete the row from db
+            db.session.delete(task)
 
-        # commit the changes to the db
-        db.session.commit()
+            # commit the changes to the db
+            db.session.commit()
 
-        return task
+            return {
+                'success': 'true',
+                'message': 'Task successfully deleted.'
+            }
+
+        except Exception as e:
+            return {
+                'success': 'false',
+                'message': '{}'.format(e)
+            }
 
 
 # bind the resource to a route
-tasks.add_resource(Tasks, '/tasks', )
+tasks.add_resource(AllTasks, '/tasks/<int:status_id>')
+tasks.add_resource(Tasks, '/task', '/task/<int:task_id>')
